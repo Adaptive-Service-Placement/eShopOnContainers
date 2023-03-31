cb940fcfca3a3d3114a43e9fe10edcec89c0aa2f
fsix <six@campus.tu-berlin.de>
2023-03-31 15:01:23 +0200

    test commit

--- deploy/k8s/helm/deploy-helm-charts.sh
+++ deploy/k8s/helm/deploy-helm-charts.sh
@@ -2,4 +2,4 @@
 
 HELM_PATH='cat deploy/k8s/helm'
 echo "####### Deploying basket-api ######"
-helm upgrade --install basket-chart $HELM_PATH/basket-api/ --values $HELM_PATH/basket-api/values.yaml
+helm upgrade --install basket-chart deploy/k8s/helm/basket-api/ --values deploy/k8s/helm/basket-api/values.yaml
