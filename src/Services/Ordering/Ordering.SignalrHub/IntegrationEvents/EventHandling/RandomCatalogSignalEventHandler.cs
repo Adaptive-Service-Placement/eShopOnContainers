78ccaf2cc79a7864686616b97062f707fd464dd6
fsix <six@campus.tu-berlin.de>
2023-05-28 15:01:37 +0200

    add latency logging in signal eventhandler

--- src/Services/Ordering/Ordering.SignalrHub/IntegrationEvents/EventHandling/RandomCatalogSignalEventHandler.cs
+++ src/Services/Ordering/Ordering.SignalrHub/IntegrationEvents/EventHandling/RandomCatalogSignalEventHandler.cs
@@ -30,5 +30,11 @@ public class RandomCatalogSignalEventHandler : IIntegrationEventHandler<RandomCa
            }
 
         }
+
+        using (LogContext.PushProperty("Latency", $"{@event.Id}-{Program.AppName}"))
+        {
+            TimeSpan latency = DateTime.Now - @event.CreationDate;
+            _logger.LogInformation("{latency}", (int)latency.TotalMilliseconds);
+        }
     }
 }
